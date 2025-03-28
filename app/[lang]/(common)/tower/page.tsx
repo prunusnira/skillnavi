39e43d467b6549bbb4eb67902894a83ea0d0f476
Nira <prunusnira@gmail.com>
2025-03-28 19:05:33 +0900

    [NAVI-41] 타워 임시페이지 추가

---
+++ app/[lang]/(common)/tower/page.tsx
@@ -0,0 +1,18 @@
+import Card from '@/common/card/Card';
+
+const PageTowerMain = () => {
+    return (
+        <Card title={'Tower'}>
+            <section className={'flex-col-center'}>
+                <div className={'text-md font-bold'}>
+                    Tower system is under construction!
+                </div>
+                <div className={'text-sm'}>
+                    The new Skill Navigator Tower System will be dropped in soon
+                </div>
+            </section>
+        </Card>
+    );
+};
+
+export default PageTowerMain;
