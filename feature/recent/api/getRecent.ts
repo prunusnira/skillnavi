e1d88af13f545e933f1eab16493a35adc93eaadf
Nira <prunusnira@gmail.com>
2025-03-30 20:58:33 +0900

    [NAVI-45] 페이지 이동 시 메뉴 닫기 처리

--- feature/recent/api/getRecent.ts
+++ feature/recent/api/getRecent.ts
@@ -3,5 +3,9 @@ import { Recent } from '@/feature/recent/data/Recent';
 import { API_RECENT } from '@/url/api';
 
 export const getRecent = async () => {
-    return await fetchAdv.get<Recent[]>(API_RECENT);
+    return await fetchAdv.get<Recent[]>(API_RECENT, {
+        next: {
+            revalidate: 0,
+        },
+    });
 };
