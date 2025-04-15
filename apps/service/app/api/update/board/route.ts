60ca8f3d3a2f3cc9cc657b8219780d7ab9de90ca
Nira <prunusnira@gmail.com>
2025-04-15 18:24:05 +0900

    [NAVI-61] 플레이어 보드 업데이트 문제 수정

--- apps/service/app/api/update/board/route.ts
+++ apps/service/app/api/update/board/route.ts
@@ -4,17 +4,25 @@ import fs from 'fs';
 
 export const POST = async (
     req: NextRequest,
-    { params }: { params: { uid: string } },
 ) => {
     return RouteWrapper({
         req,
         work: async () => {
-            const { uid } = params;
-            const blob = await req.blob();
-            const filePath = `/data/board/${uid}.png`;
-            const writer = fs.createWriteStream(filePath);
-            writer.write(blob);
-            writer.close();
+            let fd;
+            try {
+                const searchParams = req.nextUrl.searchParams;
+                const uid = searchParams.get('uid');
+                const blob = await req.text();
+                const filePath = `${process.env.NEXT_PUBLIC_BOARD}${uid}.png`;
+                const buffer = Buffer.from(blob, 'base64');
+                const fd = fs.openSync(filePath, 'w');
+                fs.writeFileSync(fd, buffer);
+                fs.close(fd);
+            } catch (error) {
+                console.log(error);
+            } finally {
+                if(fd) fs.close(fd);
+            }
 
             return NextResponse.json({});
         },
