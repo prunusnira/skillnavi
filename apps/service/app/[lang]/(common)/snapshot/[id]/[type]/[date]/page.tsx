dc2a3f744e185558c39e60e608fb1f1cdc704b11
Nira <prunusnira@gmail.com>
2025-04-21 11:49:45 +0900

    [NAVI-69] 스냅샷 스킬표기 오류 수정

--- apps/service/app/[lang]/(common)/snapshot/[id]/[type]/[date]/page.tsx
+++ apps/service/app/[lang]/(common)/snapshot/[id]/[type]/[date]/page.tsx
@@ -4,24 +4,44 @@ import { SnapshotData, SnapshotSkill } from '@/feature/snapshot/data/Snapshot';
 import { SkillForTable } from '@/feature/skill/data/Skill';
 import SkillGridForSnapshot from '@/feature/skill/component/table/SkillGridForSnapshot';
 
-const PageSnapshotDetail = async ({ params }: { params: { id: string, type: string, date: string } }) => {
+const PageSnapshotDetail = async ({
+    params,
+}: {
+    params: { id: string; type: string; date: string };
+}) => {
     const { id, type, date } = params;
     const data = await getSnapshotDetail(id, type, date);
     const json = JSON.parse(data) as SnapshotData;
 
-    const convertSnapshotToSkill = (data: SnapshotSkill[], isHot: boolean): (SkillForTable & { mname: string })[] => {
-        return data.map(item => ({
-            mid: item.mid,
-            playver: item.version,
-            patterncode: item.ptcode,
-            level: item.lv,
-            maxrank: item.rank,
-            rate: item.rate,
-            fc: item.fc === 'Y',
-            hot: isHot,
-            skill: item.skill,
-            mname: item.mname,
-        }));
+    const convertSnapshotToSkill = (
+        data: SnapshotSkill[],
+        isHot: boolean,
+    ): (SkillForTable & { mname: string })[] => {
+        return data.map((item) => {
+            // 스킬값이 예전 형식인 경우 복구 처리
+            let newSkill = item.skill;
+            if (item.skill.toString().indexOf('.') !== -1) {
+                const skill = item.skill.toString().split('.');
+                if (skill[1]?.length === 1) {
+                    newSkill = Number(`${skill.join('')}0`);
+                } else {
+                    newSkill = Number(skill.join(''));
+                }
+            }
+
+            return {
+                mid: item.mid,
+                playver: item.version,
+                patterncode: item.ptcode,
+                level: item.lv,
+                maxrank: item.rank,
+                rate: item.rate,
+                fc: item.fc === 'Y',
+                hot: isHot,
+                skill: newSkill,
+                mname: item.mname,
+            };
+        });
     };
 
     return (
@@ -43,24 +63,28 @@ const PageSnapshotDetail = async ({ params }: { params: { id: string, type: stri
                 {/* 스킬 데이터 */}
                 <div className={'text-2xl font-semibold py-[20px]'}>HOT</div>
                 <div className={'w-full grid grid-cols-3 md:grid-cols-4'}>
-                    {convertSnapshotToSkill(json.hot, true).map((item, index) => (
-                        <SkillGridForSnapshot
-                            key={item.mid}
-                            skill={item}
-                            index={index}
-                        />
-                    ))}
+                    {convertSnapshotToSkill(json.hot, true).map(
+                        (item, index) => (
+                            <SkillGridForSnapshot
+                                key={item.mid}
+                                skill={item}
+                                index={index}
+                            />
+                        ),
+                    )}
                 </div>
 
                 <div className={'text-2xl font-semibold py-[20px]'}>OTHER</div>
                 <div className={'w-full grid grid-cols-3 md:grid-cols-4'}>
-                    {convertSnapshotToSkill(json.oth, true).map((item, index) => (
-                        <SkillGridForSnapshot
-                            key={item.mid}
-                            skill={item}
-                            index={index}
-                        />
-                    ))}
+                    {convertSnapshotToSkill(json.oth, true).map(
+                        (item, index) => (
+                            <SkillGridForSnapshot
+                                key={item.mid}
+                                skill={item}
+                                index={index}
+                            />
+                        ),
+                    )}
                 </div>
             </section>
         </Card>
